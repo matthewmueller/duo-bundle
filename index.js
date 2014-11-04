901d286deddc85606c860383b74b68235bf70209
Matthew Mueller <mattmuelle@gmail.com>
2014-11-04 12:45:55 +1000

    support 0.10.x

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 /**
- * Module Dependencies
+ * Add backwards compatibility for Node 0.10.
  */
 
-var bundle = require('./lib/bundle.js');
+require('gnode');
 
 /**
- * Export `middleware`
+ * Export `Duo`.
  */
 
-module.exports = middleware;
-
-/**
- * Environment
- */
-
-var production = 'production' == process.env.NODE_ENV;
-
-/**
- * Initialize `middleware`
- *
- * @param {String} glob
- * @return {Generator}
- */
-
-function middleware(root) {
-  return function(glob) {
-    return function *(next) {
-      if (!production) yield bundle(root, glob);
-      yield next;
-    }
-  }
-}
+module.exports = require('./lib/duo-bundle');
